12791fe33b7a06f77c79c42142f1a5211de5c6ae
이용수 <dydtn61498@naver.com>
2025-04-28 11:47:55 +0900

    123

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Route, Routes} from "react-router-dom";
+
+function App() {
+
+  return (
+      <div>
+          <Routes>
+              <Route path="/" element={<>}/>
+          </Routes>
+      </div>
+  )
+}
+
+export default App
